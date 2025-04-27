1ea6e54a37db99fda8290069e50a8b58d4960778
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-04-28 01:13:54 +0300

    setup pages

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
